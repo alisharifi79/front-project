c7bb35d4ca3726ec648f0ec3a15bc6195f451aa9
Ali Sharifi <shaarifi.1379@gmail.com>
2022-07-04 00:17:03 +0430

    js files

--- Java script/vueJsUdemy/components/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-//import GreeTing from './components/Greeting.vue'
-
-const vm = createApp(App)
-
-//vm.component('GreeTing', GreeTing)
-
-vm.mount('#app')
--- Java script/vueJsUdemy/music/src/main.js
+++
@@ -1,8 +0,0 @@
-/* eslint linebreak-style: ["error", "windows"] */
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import './assets/tailwind.css';
-
-createApp(App).use(store).use(router).mount('#app');
